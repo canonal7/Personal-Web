123f8e21e7c2ffa8d7f4d91620e0ec162fe5f11e
canonal7 <55083192+canonal7@users.noreply.github.com>
2022-05-18 18:23:30 +0300

    final

--- src/pages/Main/Contact.js
+++ src/pages/Main/Contact.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {useNav} from '../../Components/CustomHooks/useNav';
 import '../../css/Pages.css'
+import github_icon from "../../github_icon.png"
 
 
 const Contact = () => {
@@ -15,12 +16,20 @@ const Contact = () => {
 	return (
 		<section ref={contactRef} id='contactCont'>
 			<img
-				src='https://source.unsplash.com/random/600x600/?nature,water'
+				src='https://images.unsplash.com/photo-1559030623-0226b1241edd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=600&q=80'
 				alt='unsplash-img'
 			/>
 			<div>
-				<h3>CONTACT</h3>
-				<p>This is the contact section</p>
+				<h3>CONTACT ME</h3>
+					<a href="mailto:can.onal@ug.bilkent.edu.tr">
+					<img className = "mailIcon"src="https://cdn-icons-png.flaticon.com/512/561/561127.png" alt="" />
+					</a>
+					<a target = "_blank" href = "https://www.linkedin.com/in/can-onal/">
+					<img className="linkedInIcon my-5" src="https://img.icons8.com/fluency/2x/linkedin-circled.png" alt="" />
+					</a>
+				<a target="_blank" href="https://github.com/canonal7">
+					<img className="icon" src={github_icon} alt="" />
+				</a>
 			</div>
 		</section>
 	);
--- src/pages/Main/Projects.js
+++ src/pages/Main/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useNav} from '../../Components/CustomHooks/useNav';
+import github_icon from "../../github_icon.png"
 import '../../css/Pages.css'
 
 const Projects = () => {
@@ -8,12 +9,15 @@ const Projects = () => {
 	return (
 		<section ref={projectsRef} id='projectsCont'>
 			<img
-				src='https://source.unsplash.com/random/600x600/?nature,water'
+				src='https://images.unsplash.com/photo-1553877522-43269d4ea984?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600'
 				alt='unsplash-img'
 			/>
 			<div>
 				<h3>PROJECTS</h3>
-				<p>This is the projects section</p>
+				<p>For my projects you can view my GitHub profile</p>
+				<a href="https://github.com/canonal7">
+					<img className = "icon" src={github_icon} alt="" />
+				</a>
 			</div>
 		</section>
 	);
